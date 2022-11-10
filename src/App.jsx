99b236b92310089d27369ca138d859a1cb5b6ad4
Mohsin <mohsin@sociabletech.com.au>
2022-11-10 23:28:42 +0500

    added default route for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import "./App.css";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Pokemon from "components/Pokemon/Index";
 import Favorite from "components/Favorites/Index";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const styles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    overflowX: "hidden",
+  };
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        overflowX: "hidden",
-      }}
-    >
+    <Box sx={styles}>
       <Container
         sx={{
           width: "100%",
@@ -26,7 +30,7 @@ function App() {
       >
         <Router>
           <Routes>
-            <Route path="/" element={<Pokemon />} />
+            <Route path="/" element={<Navigate replace to="/pokedex" />} />
             <Route path="/pokedex" element={<Pokemon />} />
             <Route path="/pokedex/favorites" element={<Favorite />} />
           </Routes>
